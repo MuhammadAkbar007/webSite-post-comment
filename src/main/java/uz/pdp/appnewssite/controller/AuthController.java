d053908b17b47c1ba2c238dd46ad372cce572a91
MuhammadAkbar <fotismenos007@mail.ru>
2021-09-21 10:30:28 +0500

    pf commit

--- src/main/java/uz/pdp/appnewssite/controller/AuthController.java
+++ src/main/java/uz/pdp/appnewssite/controller/AuthController.java
@@ -2,6 +2,7 @@ package uz.pdp.appnewssite.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
